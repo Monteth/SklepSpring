8529d2acf75eae66239e5ac539f4297263df2b68
Konrad Bosak <greezlie@gmail.com>
2019-01-18 19:09:38 +0100

    Communicate with Spring App

---
+++ src/main/java/pl/monteth/po/sklep/Beans/HelloBean.java
@@ -0,0 +1,25 @@
+package pl.monteth.po.sklep.Beans;
+
+public class HelloBean {
+
+    private String message;
+
+    public HelloBean(String message){
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
--- src/main/java/pl/monteth/po/sklep/Controllers/HelloWorldController.java
+++ src/main/java/pl/monteth/po/sklep/Controllers/HelloWorldController.java
@@ -1,16 +1,28 @@
 package pl.monteth.po.sklep.Controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import pl.monteth.po.sklep.Beans.HelloBean;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class HelloWorldController {
-    @RequestMapping(method = RequestMethod.GET, path = "/hello")
-    //@GetMapping(path = "/hello")  // same result
+    //@RequestMapping(method = RequestMethod.GET, path = "/hello")
+    @GetMapping(path = "/hello")  // same result
     public String hello(){
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-bean")
+    public HelloBean helloBean(){
+        return new HelloBean("Witajcie klienci, pochodzę z servera!");
+    }
+
+    @GetMapping(path = "/hello/path-variable/{name}")
+    public HelloBean helloPath(@PathVariable String name){
+        return new HelloBean("Hello " + name);
     }
 
 }
